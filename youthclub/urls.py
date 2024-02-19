02999f4d97829a9abc5ea7d2002cf4b08f383d41
Lydia Young <lydsjoyy@gmail.com>
2024-02-19 22:06:44 +0000

    readies code for deploy

--- youthclub/urls.py
+++ youthclub/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from registration.views import book
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registration/', book, name='registration'),
 ]
