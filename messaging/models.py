4b90be1c46b43823323c4e83f5e566c47414314c
Lydia Young <lydsjoyy@gmail.com>
2024-02-21 19:03:27 +0000

    creating messaging model

---
+++ messaging/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# From https://reintech.io/blog/building-a-messaging-system-in-django
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    subject = models.CharField(max_length=200)
+    body = models.TextField()
