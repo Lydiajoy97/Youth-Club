9656077cb93c3079c32a51c66471fd3f4ba79efd
Lydia Young <lydsjoyy@gmail.com>
2024-02-21 23:05:34 +0000

    add to home page

--- registration/views.py
+++ registration/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def upload_file(request):
+def upload_form(request):
     if request.method == "POST":
         form = ModelFormWithFileField(request.POST, request.FILES)
         if form.is_valid():
