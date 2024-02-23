e3750b3823fff1b03782bc46901b6c5eafa1595b
Lydia Young <lydsjoyy@gmail.com>
2024-02-23 13:47:29 +0000

    add about page model

---
+++ about/admin.py
@@ -0,0 +1,10 @@
+# from code insitute walkthrough
+from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
---
+++ about/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from .models import about
+
+# Create your views here.
