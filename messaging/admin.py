9b669c900546e4afcd8229312285585d12e6d407
Lydia Young <lydsjoyy@gmail.com>
2024-02-25 16:01:40 +0000

    add url paths for message app

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(Message)
+class AboutAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
--- youthclub/settings.py
+++ youthclub/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'registration', 
     'messaging',
-    'about'
+    'about',
 ]
 
 MIDDLEWARE = [
