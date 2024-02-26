e13e2541853f44b6c4d8a9a3c10a33bedb53b87e
Lydia Young <lydsjoyy@gmail.com>
2024-02-26 23:58:29 +0000

    edit form.html

--- registration/models.py
+++ registration/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
 class form(models.Model):
-    first_name = models.CharField(max_length=200)
+    first_name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="form_upload")
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
     age = models.CharField(max_length=20, default=10)
     group_attending = models.CharField(max_length=200, default="The Hub")
     photo_consent = models.BooleanField(default=True)
     addional_information = models.CharField(max_length=700, blank=True)
-    trip_consent_letter = models.FileField(blank=True)
+    consent_letter = models.FileField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.first_name
--- registration/views.py
+++ registration/views.py
@@ -7,7 +7,16 @@ from django.contrib.auth.models import User
 def home(request):
     return render(request, 'index.html')
 
-def form(request):
-    User._meta.get_fields()
-    return render(request, 'form.html')
+def form_upload(request):
+
+    if request.method=="POST":
+       form = form._meta.get_fields()
+       if form.is_valid(): 
+            form.save()           
+            return HttpResponse("Thanks for uploading your form! We will be in touch soon.") 
+
+    else: 
+        form = form_upload()
+
+    return render(request,'form.html',{ 'form': form })
 
