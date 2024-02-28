dfa1a123403a23489bbf3f763a9fcb082718471d
Lydia Young <lydsjoyy@gmail.com>
2024-02-28 22:25:54 +0000

    add credit

--- registration/admin.py
+++ registration/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import UploadForm
 
 # Register your models here.
+# https://docs.djangoproject.com/en/5.0/ref/contrib/admin/
 @admin.register(UploadForm)
 class UploadFormAdmin(admin.ModelAdmin):
     pass 
