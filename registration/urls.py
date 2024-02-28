ba109f85141f6ee47ee97f10bd4597ae6f4972d4
Lydia Young <lydsjoyy@gmail.com>
2024-02-28 20:28:45 +0000

    !

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
         path('', views.home, name='home'),
-        path('form/', views.UploadForm, name='form'),
+        path('form/', views.upload_view, name='form'),
 ]
