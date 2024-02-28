e419c432a3208285a4936a3debac84890de0313e
Lydia Young <lydsjoyy@gmail.com>
2024-02-28 21:25:38 +0000

    edit forms

--- registration/forms.py
+++ registration/forms.py
@@ -1,13 +1,9 @@
 # from geeks for geeks
 from django import forms
+from django.forms import ModelForm
+from .models import UploadForm 
 
-class UploadForm(forms.Form):
-
-    first_name = forms.CharField(max_length=200)
-    last_name = forms.CharField(max_length=200)
-    email = forms.EmailField(max_length=254)
-    age = forms.CharField(max_length=20)
-    group_attending = forms.CharField(max_length=200)
-    photo_consent = forms.BooleanField()
-    addional_information = forms.CharField(max_length=700)
-    consent_letter = forms.FileField()
+class ChildrensForm(ModelForm):
+    class Meta:
+        model = UploadForm
+        fields = '__all__'
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import UploadForm
+from .forms import ChildrensForm
+from .models import UploadForm
 
 # Help to write code From https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Home_page and walkthrough.
 # https://docs.djangoproject.com/en/5.0/intro/tutorial03/ 
@@ -10,8 +11,14 @@ def home(request):
 
 # from geeks for geeks 
 def upload_view(request):
-    context ={}
-    context['form']= UploadForm()
+
+    form = ChildrensForm()
+    if request.method == "POST":
+       # print("printing POST:", request.POST)
+       form = ChildrensForm(request.POST)
+       if form.is_valid():
+        form.save()
+        return redirect ('/')
+        
+    context = {'form' : form}
     return render(request, "form.html", context)
-
-
