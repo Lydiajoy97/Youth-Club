4809f228b2c041716f8c74f2cdf1a602f8d3f232
Lydia Young <lydsjoyy@gmail.com>
2024-06-25 09:10:15 +0000

    add veiws for activites page

--- activites/views.py
+++ activites/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def activities(request):
+    """
+    Renders the About page
+    """
+    activities = activities.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "activities.html",
+        {"activities": activities},
+    )
