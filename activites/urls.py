b811bc6217aab618d673217a8150de33d1aad890
Lydia Young <lydsjoyy@gmail.com>
2024-07-01 19:35:21 +0000

    add edit fuctionality and suggestion.js page so that a user can edit posts

--- activites/urls.py
+++ activites/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.ActivityFormList.as_view(), name='activityform'),
     path('addactivity/', views.HaveYourSay.as_view(), name='addactivity'),
+     path('<slug:slug>/edit_suggestion/<int:suggestion_id>',
+         views.suggestion_edit, name='edit_suggestion'),
 ]
--- activites/views.py
+++ activites/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from .models import ActivityForm
 from .forms import HaveYourSayForm
+from django.http import HttpResponseRedirect
 
 class ActivityFormList(generic.ListView):
     queryset = ActivityForm.objects.filter(status=1)
@@ -14,3 +15,21 @@ class HaveYourSay(generic.CreateView):
     template_name = "activites/addactivity.html"
     success_url = '/'
     success_message = "Your suggestion has been submitted and is awaiting approval."
+
+def suggestion_edit(request, slug):
+    if request.method == "POST":
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        suggestion = get_object_or_404(ActivityForm, pk=activity_id)
+        activity_form = ActivityForm(data=request.POST, instance=comment)
+
+        if activity_form.is_valid() and suggestion.author == request.user:
+            suggestion = activity_form.save(commit=False)
+            suggestion.post = post
+            suggestion.approved = False
+            suggestion.save()
+            messages.add_message(request, messages.SUCCESS, 'Your suggestion has been Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating your suggestion!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
