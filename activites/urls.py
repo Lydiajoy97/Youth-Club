7aaaa532a95bbc2bc3275d5a6025f1130ab705a1
Lydia Young <lydsjoyy@gmail.com>
2024-07-07 18:35:58 +0000

    delete activity detail viewm url and page

--- activites/urls.py
+++ activites/urls.py
@@ -4,7 +4,6 @@ from .views import UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', views.ActivityFormList.as_view(), name='activityform'),
-    path('activites/<int:pk>', views.ActivityDetailView.as_view(), name='activity_detail' ),
     path('addactivity/', views.HaveYourSay.as_view(), name='addactivity'),
     path('edit/<int:pk>', UpdatePostView.as_view(), name='editactivity'),
     path('activites/<int:pk>/delete', DeletePostView.as_view(), name='deleteactivity'),
--- activites/views.py
+++ activites/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 from .models import ActivityForm
 from .forms import HaveYourSayForm
 from django.http import HttpResponseRedirect
@@ -10,10 +10,6 @@ class ActivityFormList(ListView):
     template_name = "activityform_list.html"
     paginate_by = 6
 
-class ActivityDetailView(DetailView):
-    model: ActivityForm
-    template_name = 'activity_detail.html'
-
 # From youtube 
 class HaveYourSay(CreateView):
     form_class = HaveYourSayForm 
@@ -22,45 +18,6 @@ class HaveYourSay(CreateView):
     def my_view(request):
         if ActivityForm.is_valid():
             messages.success(request, 'Form submission successful')
-
-def activity_detail(request, slug):
-
-    queryset = ActivityForm.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-    activity = post.activity.all().order_by("-created_on")
-    activity_count = post.activitys.filter(approved=True).count()
-    if request.method == "POST":
-        activity_form = ActivityForm(data=request.POST)
-        if activity_form.is_valid():
-            activity = activity_form.save(commit=False)
-            activity.name = request.user
-            activity.post = post
-            activity.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Suggestion submitted and awaiting approval'
-            )
-
-    activity_form = ActivityForm()
-
-
-# def activity_edit(request, slug):
-#     if request.method == "POST":
-#         queryset = ActivityForm.objects.filter(status=1)
-#         post = get_object_or_404(queryset, slug=slug)
-#         activity = get_object_or_404(ActivityForm, pk=activity_id)
-#         activity_form = ActivityForm(data=request.POST, instance=activity)
-
-#         if activity_form.is_valid() and activity.name == request.user:
-#             activity = activity_form.save(commit=False)
-#             activity.post = post
-#             activity.approved = False
-#             activity.save()
-#             messages.add_message(request, messages.SUCCESS, 'Your suggestion has been Updated!')
-#         else:
-#             messages.add_message(request, messages.ERROR, 'Error updating your suggestion!')
-
-#     return HttpResponseRedirect(reverse('addactivity', args=[slug]))
 
 class UpdatePostView(UpdateView):
     model = ActivityForm
