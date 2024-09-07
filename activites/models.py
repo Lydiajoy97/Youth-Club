6e1865de75e24f0cce6734143f2e73904f6bf08b
Lydia Young <lydsjoyy@gmail.com>
2024-09-07 14:50:17 +0000

    delete database and reapply migrations to fix migration wrrors

--- activites/models.py
+++ activites/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.conf import settings
+import django.db.models.deletion
 from django.contrib.auth.models import User
+import datetime
+import django
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class ActivityForm(models.Model):
-    first_name = models.CharField(max_length=200, default="Pick a display name")
+    first_name = models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='activity_form', to=settings.AUTH_USER_MODEL),
     email = models.EmailField(max_length=254)
     game_ideas = models.TextField()
     approved_by = models.BooleanField(default=False)
     status = models.IntegerField(choices=STATUS, default=0)
-    updated_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(default=datetime.datetime.now)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="addactivity",
+        User, on_delete=models.CASCADE, related_name="addactivity", default=0,
     )
     
     class Meta:
