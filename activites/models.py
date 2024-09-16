0e93bbcdb2638d54a91356da1d4f8b7320e846d5
Lydia Young <lydsjoyy@gmail.com>
2024-09-16 18:12:59 +0000

    Edit model.py in activity forms to remove field first_name

--- activites/models.py
+++ activites/models.py
@@ -10,7 +10,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class ActivityForm(models.Model):
-    first_name = models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='activity_form', to=settings.AUTH_USER_MODEL),
     email = models.EmailField(max_length=254)
     game_ideas = models.TextField()
     approved_by = models.BooleanField(default=False)
@@ -22,6 +21,3 @@ class ActivityForm(models.Model):
     
     class Meta:
         ordering = ["updated_on"]
-
-    def __str__(self):
-        return f"ActivityForm {self.game_ideas} by {self.first_name}"
